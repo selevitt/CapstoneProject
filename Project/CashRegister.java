a784d81bbf1b2761ab009df3c6f3a6ba724addf2
selevitt <selevitt@student.naperville203.org>
2015-04-23 15:05:01 -0500

    Implemented Buttons for the menu

--- Project/CashRegister.java
+++ Project/CashRegister.java
@@ -1,4 +1,9 @@
-
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
 
 /**
  * Write a description of class CashRegister here.
@@ -13,7 +18,33 @@ public class CashRegister
     {
         Directory itemList = new Directory();
         
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
         
+        frame.add(panel);
+        
+        JButton b1 = new JButton();
+        JButton b2 = new JButton();
+        JButton b3 = new JButton();
+        frame.setSize(400,200);     
+        
+        b1.setVisible(true);
+        b1.setText("Start Checkout");
+        panel.add(b1);
+        
+        b2.setVisible(true);
+        b2.setText("Add Items");
+        panel.add(b2);
+        
+        b3.setVisible(true);
+        b3.setText("Change Prices");
+        panel.add(b3);
+        
+        frame.setVisible(true);
+        private class ClickListener implements ActionListener
+        {
+                                                                                                               
+        }
     }
     
     
